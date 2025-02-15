c7d7af2ed9f85b6d92c7f98587506c891ae0d15b
zethboy <yoginugraha842@gmail.com>
2025-02-15 11:51:44 +0700

    welcome section

--- particles.js
+++ particles.js
@@ -20,10 +20,10 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 800,
+        value: 400,
         density: {
           enable: true,
-          value_area: 800
+          value_area: 400
         }
       },
       color: {
